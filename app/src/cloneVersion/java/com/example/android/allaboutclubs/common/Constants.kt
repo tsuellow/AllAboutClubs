a21f194d745fc9c9fc5800984c190dcfbb449c2f
tsuellow <45243220+tsuellow@users.noreply.github.com>
2023-04-14 23:27:09 +0200

    flavours tweeked

---
+++ app/src/cloneVersion/java/com/example/android/allaboutclubs/common/Constants.kt
@@ -0,0 +1,14 @@
+package com.example.android.allaboutclubs.common
+
+class Constants {
+    companion object{
+        /**
+         * here we could use an alternative API URL for the clone app
+         */
+        const val PLACEHOLDER_URL = "https://public.allaboutapps.at/"
+
+        const val UNKNOWN_ERROR="UNKNOWN ERROR"
+        const val NETWORK_ERROR="NETWORK ERROR"
+        const val IO_ERROR="IO ERROR"
+    }
+}
---
+++ app/src/originalVersion/java/com/example/android/allaboutclubs/common/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.android.allaboutclubs.common
+
+class Constants {
+    companion object{
+        const val PLACEHOLDER_URL = "https://public.allaboutapps.at/"
+
+        const val UNKNOWN_ERROR="UNKNOWN ERROR"
+        const val NETWORK_ERROR="NETWORK ERROR"
+        const val IO_ERROR="IO ERROR"
+    }
+}
